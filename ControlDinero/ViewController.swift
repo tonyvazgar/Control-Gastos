71b36d72c4a4eb642c69bd0bcf39d3e387ac4570
Tony <tonyvazgar@Tonys-MacBook-Pro.local>
2020-05-17 23:53:50 -0500

    Primer commit

---
+++ ControlDinero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ControlDinero
+//
+//  Created by Tony Vazgar on 17/05/20.
+//  Copyright © 2020 Tony Vazgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
