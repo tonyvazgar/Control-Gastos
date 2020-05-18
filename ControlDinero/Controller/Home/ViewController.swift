cb4a23fbe65e20f017129718fa5d53eec5385ccf
Tony <tonyvazgar@Tonys-MacBook-Pro.local>
2020-05-18 17:41:59 -0500

    Nuevos elementos UI

---
+++ ControlDinero/Controller/Home/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  ControlDinero
+//
+//  Created by Tony Vazgar on 17/05/20.
+//  Copyright © 2020 Tony Vazgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Declaración de variables
+    @IBOutlet weak var labelMes: UILabel!
+    @IBOutlet weak var labelGanancias: UILabel!
+    @IBOutlet weak var labelGastos: UILabel!
+    @IBOutlet weak var labelTotal: UILabel!
+    @IBOutlet weak var quincenasButton: UIButton!
+    @IBOutlet weak var gastosButton: UIButton!
+    
+    // MARK: Actions
+    @IBAction func buttonQuincenas(_ sender: UIButton) {
+        sender.shine()
+        sender.jump()
+    }
+    
+    @IBAction func buttonGastos(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    // MARK: Funciones
+    private func getCurrentDate() -> String {
+        let now = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        let nameOfMonth = dateFormatter.string(from: now)
+        dateFormatter.dateFormat = "yyyy"
+        let yearString = dateFormatter.string(from: now)
+        return "\(nameOfMonth) \(yearString)"
+    }
+    
+    private func getGanancias() -> String {
+        var ganancias = "$33,204.33"
+        return ganancias
+    }
+    
+    private func getGastos() -> String {
+        var gastos = "$10,000.33"
+        return gastos
+    }
+    
+    private func getTotal() -> String {
+        var total = "$23,204.00"
+        return total
+    }
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        quincenasButton.round()
+        quincenasButton.bounce()
+        quincenasButton.jump()
+        gastosButton.round()
+        gastosButton.jump()
+        gastosButton.bounce()
+        
+        //Obtener datos para poner en labels
+        labelMes.text = getCurrentDate()
+        labelGastos.text = getGastos()
+        labelGanancias.text = getGanancias()
+        labelTotal.text = getTotal()
+    }
+
+
+}
+
---
+++ ControlDinero/Util/UIButtonExtension.swift
@@ -0,0 +1,59 @@
+//
+//  UIButtonExtension.swift
+//  ControlDinero
+//
+//  Created by Tony Vazgar on 18/05/20.
+//  Copyright © 2020 Tony Vazgar. All rights reserved.
+//
+
+import UIKit
+
+private let orange = UIColor(red: 254/255, green: 148/255, blue: 0/255, alpha: 1 )
+
+extension UIButton {
+    
+    // MARK: Borde redondo
+    func round() {
+        layer.cornerRadius = bounds.height / 2
+        clipsToBounds = true
+    }
+    
+    // MARK:  Rebote
+    func bounce() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }) { (completion) in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.transform = .identity
+            })
+        }
+    }
+    
+    // MARK:  Brilla
+    func shine() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.alpha = 0.5
+        }) { (completion) in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.alpha = 1
+            })
+        }
+    }
+    
+    // MARK:  Salta
+    func jump() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.transform = CGAffineTransform(translationX: 0, y: -10)
+        }) { (completion) in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.transform = .identity
+            })
+        }
+    }
+    
+    func selectOperation(_ selected: Bool){
+        backgroundColor = selected ? .white: orange
+        setTitleColor(selected ? orange: .white, for: .normal)
+    }
+    
+}
