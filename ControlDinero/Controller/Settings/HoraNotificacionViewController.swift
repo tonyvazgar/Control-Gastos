d6c546bdffbcac4ca70122f990dce34211048c5f
Tony <tonyvazgar@Tony-Vazgars-MacBook-Pro.local>
2020-09-11 00:02:53 -0500

    implementando notificaciones

--- ControlDinero/Controller/Settings/HoraNotificacionViewController.swift
+++ ControlDinero/Controller/Settings/HoraNotificacionViewController.swift
@@ -10,4 +10,41 @@ import UIKit
 
 class HoraNotificacionViewController: UIViewController {
     
+    @IBOutlet weak var timePiccker: UIDatePicker!
+    @IBOutlet weak var defaultButton: UIButton!
+    @IBOutlet weak var setButton: UIButton!
+    
+    
+    @IBAction func defaultButtonAction(_ sender: Any) {
+        
+        print("")
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        dateFormatter.dateFormat = "HH"
+        let hours = dateFormatter.string(from: timePiccker.date)
+        print(hours)
+        
+        dateFormatter.dateFormat = "mm"
+        let minutes = dateFormatter.string(from: timePiccker.date)
+        print(minutes)
+    }
+    
+    @IBAction func setButtonAction(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemGroupedBackground
+        
+
+    
+        defaultButton.bounce()
+        defaultButton.round()
+        
+        setButton.round()
+        setButton.bounce()
+    }
 }
